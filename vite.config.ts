2d2f43861a4d51e5bf619b7e02422956924e7935
frank <frank@emtek.com>
2026-06-25 15:26:22 +0800

    1.因自訂網域更新套件

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       plugins: [react()],
-      base: '/mission-ready/', 
+      base: process.env.CF_PAGES ? '/' : '/mission-ready/', 
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
